1f729851dd09eef6bc432023f43754b7205c8b15
S M <hubble.stein@gmail.com>
2023-05-10 18:36:07 +0900

    scale, translate, reset

--- mandelbrot_set/main.cpp
+++ mandelbrot_set/main.cpp
@@ -18,17 +18,27 @@
 //        color : = palette[iteration]
 //        plot(Px, Py, color)
 
-import <cmath>;
 #include <SFML/Graphics.hpp>
 
-void make_pixels(sf::VertexArray& va, const unsigned width, const unsigned height);
+constexpr double xMin{ -2.0 };
+constexpr double xMax{ 0.47 };
+constexpr double yMin{ -1.12 };
+constexpr double yMax{ 1.12 };
+constexpr unsigned maxIteration{ 1000 };
+constexpr unsigned width{ 800 };
+constexpr unsigned height{ 800 };
+
+void make_pixels(sf::VertexArray& va);
+void update_colors(sf::VertexArray& va, const double scale, const double dx, const double dy);
+unsigned get_color(const double px, const double py, const double scale = 1.0, const double dx = 0.0, const double dy = 0.0);
 
 int main()
 {
-    constexpr unsigned width{ 800 };
-    constexpr unsigned height{ 800 };
+    double scale{ 1.0 };
+    double deltaX{};
+    double deltaY{};
     sf::VertexArray pixels{sf::Points};
-    make_pixels(pixels, width, height);
+    make_pixels(pixels);
 
     sf::RenderWindow window(sf::VideoMode(width, height), "Mandelbrot Set", sf::Style::Titlebar | sf::Style::Close);
     window.setFramerateLimit(60);
@@ -40,6 +50,52 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::I)) // zoom in
+            {
+                scale *= 1.5;
+                update_colors(pixels, scale, deltaX, deltaY);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::O)) // zoom out
+            {
+                if (scale == 1.0)
+                {
+                    continue;
+                }
+
+                scale /= 1.5;
+                update_colors(pixels, scale, deltaX, deltaY);
+                if (scale < 1.0)
+                {
+                    scale = 1.0;
+                }
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            {
+                deltaX += 0.1;
+                update_colors(pixels, scale, deltaX, deltaY);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            {
+                deltaX -= 0.1;
+                update_colors(pixels, scale, deltaX, deltaY);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+            {
+                deltaY += 0.1;
+                update_colors(pixels, scale, deltaX, deltaY);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+            {
+                deltaY -= 0.1;
+                update_colors(pixels, scale, deltaX, deltaY);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::R)) // reset
+            {
+                scale = 1.0;
+                deltaX = 0.0;
+                deltaY = 0.0;
+                update_colors(pixels, scale, deltaX, deltaY);
+            }
         }
 
         window.clear();
@@ -51,38 +107,51 @@ int main()
 
 }
 
-void make_pixels(sf::VertexArray& va, const unsigned width, const unsigned height)
+void make_pixels(sf::VertexArray& va)
 {
-    constexpr double xMin{ -2.0 };
-    constexpr double xMax{ 0.47 };
-    constexpr double yMin{ -1.12 };
-    constexpr double yMax{ 1.12 };
-    constexpr unsigned maxIteration{ 1000 };
-
     for (unsigned px{}; px < width; ++px)
     {
         for (unsigned py{}; py < height; ++py)
         {
-            double x0{ (xMax - xMin) * px / width + xMin };
-            double y0{ (yMax - yMin) * py / height + yMin };
+            unsigned color{ get_color(px, py) };
 
-            double x{};
-            double y{};
-            unsigned iteration{};
-            while (x * x + y * y <= 4.0 && iteration < maxIteration)
-            {
-                double xtemp{ x * x - y * y + x0 };
-                y = 2.0 * x * y + y0;
-                x = xtemp;
-                ++iteration;
-            }
             va.append(sf::Vertex{
                 sf::Vector2f{static_cast<float>(px), static_cast<float>(py)},
                 sf::Color{
-                    static_cast<sf::Uint8>(iteration / 256),
-                    static_cast<sf::Uint8>(iteration % 256),
+                    static_cast<sf::Uint8>(color / 256),
+                    static_cast<sf::Uint8>(color % 256),
                     0}}
             );
         }
     }
+}
+
+void update_colors(sf::VertexArray& va, const double scale, const double dx, const double dy)
+{
+    for (size_t i{}; i < va.getVertexCount(); ++i)
+    {
+        unsigned color{ get_color(va[i].position.x, va[i].position.y, scale, dx, dy) };
+
+        va[i].color.r = static_cast<sf::Uint8>(color / 256);
+        va[i].color.g = static_cast<sf::Uint8>(color % 256);
+    }
+}
+
+unsigned get_color(const double px, const double py, const double scale, const double dx, const double dy)
+{
+    double x0{ ((xMax - xMin) * px / width + xMin + dx) / scale};
+    double y0{ ((yMax - yMin) * py / height + yMin + dy) / scale};
+
+    double x{};
+    double y{};
+    unsigned iteration{};
+    while (x * x + y * y <= 4.0 && iteration < maxIteration)
+    {
+        double xTemp{ x * x - y * y + x0 };
+        y = 2.0 * x * y + y0;
+        x = xTemp;
+        ++iteration;
+    }
+
+    return iteration;
 }
